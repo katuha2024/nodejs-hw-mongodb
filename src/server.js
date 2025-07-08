7a30aa0b123b050f89d3ac898187cc0eb77ddbe6
katuha2024 <ukrbrend2020@gmail.com>
2025-07-08 06:08:53 +0300

    hw3

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import { handleGetAllContacts } from './controllers/contactsController.js';
-import { getContactById } from './controllers/contactsController.js';
+
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
   app.use(pino());
+  app.use(express.json()); 
 
-  
-  app.get('/contacts', handleGetAllContacts);
-  app.get('/contacts/:contactId', getContactById);
+  app.use('/contacts', contactsRouter);
 
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });
+  app.use(notFoundHandler);  
+  app.use(errorHandler);     
 
   const PORT = process.env.PORT || 3000;
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,10 +1,23 @@
 import Contact from '../models/contactModel.js';
 
 export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
+  return await Contact.find();
 };
 
 export const findContactById = async (contactId) => {
   return await Contact.findById(contactId);
 };
+
+export const addContact = async (contactData) => {
+  return await Contact.create(contactData);
+};
+export const updateContactById = async (contactId, updateData) => {
+  return await Contact.findByIdAndUpdate(contactId, updateData, {
+    new: true,
+    runValidators: true,
+  });
+};
+export const deleteContactById = async (contactId) => {
+  return await Contact.findByIdAndDelete(contactId);
+};
+
