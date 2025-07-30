bb1e61fe238ecdbd873632cd75c6948bb5b2ae6e
katuha2024 <ukrbrend2020@gmail.com>
2025-07-30 06:02:30 +0300

    hw6

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -98,3 +98,26 @@ export const deleteContact = async (req, res) => {
 
   res.status(204).send();
 };
+
+export const updateContactPhoto = async (req, res) => {
+  const userId = req.user._id;
+  const { contactId } = req.params;
+
+  if (!req.file) {
+    throw createError(400, 'No file uploaded');
+  }
+
+  const photoUrl = await uploadToCloudinary(req.file.buffer, req.file.originalname);
+
+  const updatedContact = await updateContactById(contactId, { photo: photoUrl }, userId);
+
+  if (!updatedContact) {
+    throw createError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: 'Photo updated successfully',
+    data: updatedContact,
+  });
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,17 +5,18 @@ import {
   createContact,
   updateContact,
   deleteContact,
+  updateContactPhoto,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../schemas/contactSchemas.js';
-import { authenticate } from '../middlewares/authenticate.js'; 
-
+import { authenticate } from '../middlewares/authenticate.js';
+import { upload } from '../middlewares/uploadMiddleware.js'; 
 
 const router = express.Router();
 
-router.use(authenticate); 
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(handleGetAllContacts));
 
@@ -24,6 +25,14 @@ router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
 router.post('/', validateBody(createContactSchema), ctrlWrapper(createContact));
 
 router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContact));
+
+
+router.patch(
+  '/:contactId/photo',
+  isValidId,
+  upload.single('photo'), 
+  ctrlWrapper(updateContactPhoto)
+);
 
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContact));
 
