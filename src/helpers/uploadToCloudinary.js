42250f4f9d5b42139daa976239959fe2db6e7e64
katuha2024 <ukrbrend2020@gmail.com>
2025-08-03 14:07:35 +0300

    fix

--- src/helpers/uploadToCloudinary.js
+++ src/helpers/uploadToCloudinary.js
@@ -1,6 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { Readable } from 'stream';
 
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
 export const uploadToCloudinary = (fileBuffer, fileName) => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
